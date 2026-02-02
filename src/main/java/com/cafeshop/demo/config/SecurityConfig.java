c4749b19f8b7bd77db987ad1706f70ecb1b76686
Min Pyae Hein <minpyaehein536h@prestigein.com>
2026-02-02 17:09:56 +0700

    fix the Auth request

--- src/main/java/com/cafeshop/demo/config/SecurityConfig.java
+++ src/main/java/com/cafeshop/demo/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/api/admin/menu-items").permitAll()
+                        .requestMatchers("/api/auth/**", "/api/admin/menu-items","/api/payments/webhook/omise").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider)
